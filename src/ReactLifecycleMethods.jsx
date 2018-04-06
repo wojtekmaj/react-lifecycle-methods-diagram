eb23d5ff73f263b919777731ff21d68d83dc6596
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-04-06 02:27:51 +0200

    Workaround for Google Chrome CSS grid sizing bug

--- src/ReactLifecycleMethods.jsx
+++ src/ReactLifecycleMethods.jsx
@@ -17,7 +17,24 @@ export default class ReactLifecycleMethods extends Component {
     const { advanced } = this.state;
 
     return (
-      <div className="ReactLifecycleMethods">
+      <div
+        className="ReactLifecycleMethods"
+        ref={(ref) => {
+          /**
+           * Workaround for Google Chrome bug that causes grid to jump when hovered
+           * after each rerender. Seems like Chrome can't figure out proper sizes until
+           * we give it width explicitly.
+           */
+          if (!ref) { return; }
+          requestAnimationFrame(() => {
+            /* eslint-disable no-param-reassign */
+            ref.style.width = `${ref.clientWidth}px`;
+            requestAnimationFrame(() => {
+              ref.style.width = null;
+            });
+          });
+        }}
+      >
         <Options advanced={advanced} toggleAdvanced={this.toggleAdvanced} />
         <DiagramWithLegend advanced={advanced} />
       </div>

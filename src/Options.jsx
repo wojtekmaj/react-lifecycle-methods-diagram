620b07eb35cd12b93ae4287ca28f46241d89c439
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-04-06 16:21:17 +0200

    Change "show advanced" to "Show less common lifecycles"

--- src/Options.jsx
+++ src/Options.jsx
@@ -7,7 +7,7 @@ const Options = ({ advanced, toggleAdvanced }) => (
   <fieldset className="Options">
     <legend>Options</legend>
     <input type="checkbox" id="showAdvanced" checked={advanced} onChange={toggleAdvanced} />
-    <label htmlFor="showAdvanced">Show advanced</label>
+    <label htmlFor="showAdvanced">Show less common lifecycles</label>
   </fieldset>
 );
 

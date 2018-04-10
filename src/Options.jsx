8c1d2a44bf55babc07bb83a3b6c3b6e0181f51e0
Igor Kamyshev <garik.novel@gmail.com>
2018-04-10 10:29:34 +0700

    Fix confusing wording

--- src/Options.jsx
+++ src/Options.jsx
@@ -7,7 +7,7 @@ const Options = ({ advanced, toggleAdvanced }) => (
   <fieldset className="Options">
     <legend>Options</legend>
     <input type="checkbox" id="showAdvanced" checked={advanced} onChange={toggleAdvanced} />
-    <label htmlFor="showAdvanced">Show less common lifecycles</label>
+    <label htmlFor="showAdvanced">Show me also those lifecycles which are less common</label>
   </fieldset>
 );
 

c4792a139848adf1d8b3275cf776a4520b140d38
klujanrosas <elgenio.03@gmail.com>
2018-04-11 17:46:44 -0500

    Add es-ES 🇪🇸

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -5,6 +5,7 @@ export const defaultLocale = 'en-US';
 export const languageFiles = {
   'pl-PL': import('./pl-PL.json'),
   'pt-BR': import('./pt-BR.json'),
+  'es-ES': import('./es-ES.json'),
 };
 
 const supportedLocales = [

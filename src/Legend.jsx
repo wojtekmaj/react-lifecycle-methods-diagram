2a0be62550f0d26a3c59e929d13d57ad1685fcd3
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-06-27 11:32:40 +1000

    Refactor Legend component, make Phases title translatable

--- src/Legend.jsx
+++ src/Legend.jsx
@@ -1,39 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import T from './i18n';
+
 import Phase from './legendElements/Phase';
 
-const Legend = ({ advanced }) => (advanced ? (
-  <>
-    <h2 className="hidden">
-      Phases
-    </h2>
-    <Phase key="render" name="Render" row={1} rowspan={4}>
-      Pure and has no side effects. May be paused, aborted or restarted by React.
-    </Phase>
-    <Phase key="pre-commit" name="Pre-commit" row={5} rowspan={1}>
-      Can read the DOM.
-    </Phase>
-    <Phase key="commit" last name="Commit" row={6} rowspan={2}>
-      Can work with DOM, run side effects, schedule updates.
-    </Phase>
-  </>
-) : (
-  <>
-    <h2 className="hidden">
-      Phases
-    </h2>
-    <Phase key="render" name="Render" row={1} rowspan={2}>
-      Pure and has no side effects. May be paused, aborted or restarted by React.
-    </Phase>
-    <Phase key="commit" last name="Commit" row={3} rowspan={2}>
-      Can work with DOM, run side effects, schedule updates.
-    </Phase>
-  </>
-));
+export default function Legend({ advanced }) {
+  return (
+    <>
+      <h2 className="hidden">
+        <T>Phases</T>
+      </h2>
+      <Phase key="render" name="Render" row={1} rowspan={advanced ? 4 : 2}>
+        Pure and has no side effects. May be paused, aborted or restarted by React.
+      </Phase>
+      {advanced && (
+        <Phase key="pre-commit" name="Pre-commit" row={5} rowspan={1}>
+          Can read the DOM.
+        </Phase>
+      )}
+      <Phase key="commit" last name="Commit" row={advanced ? 6 : 3} rowspan={2}>
+        Can work with DOM, run side effects, schedule updates.
+      </Phase>
+    </>
+  );
+}
 
 Legend.propTypes = {
   advanced: PropTypes.bool,
 };
-
-export default Legend;

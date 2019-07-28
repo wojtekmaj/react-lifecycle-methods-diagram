988215b322b321fae52fe484b82319199bb3f80e
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-07-28 14:27:00 +0200

    Use country-code-to-flag-emoji instead of a custom solution

--- src/Options.jsx
+++ src/Options.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import countryCodeToFlagEmoji from 'country-code-to-flag-emoji';
 
 import './Options.less';
 
 import T from './i18n';
 import { supportedReactVersions, isReactVersion } from './propTypes';
 import { supportedLocales } from './i18n/i18n';
-import { countryCodeToFlagEmoji } from './shared/utils';
 
 const SelectOption = ({
   onChange,
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
 const isUppercase = letter => /[A-Z]/.test(letter);
 
 // Splits camelCase with ZWJ characters to help with line breaking
@@ -7,14 +9,3 @@ export const splitUpperCase = str => (str
     .reduce((res, letter) => res + (isUppercase(letter) ? `\u00ad${letter}` : letter), '')
   : str
 );
-
-// Translates 'a' to '🇦', 'b' to '🇧' and so on.
-const letterToLetterEmoji = letter => String.fromCodePoint(
-  letter.toLowerCase().charCodeAt() + 127365,
-);
-
-// Translates 'pl' to 'PL', 'en-US' to 'US' and so on.
-const countryCodeToCountry = countryCode => countryCode.split('-').pop().toUpperCase();
-
-// Translates 'pl-PL' to 🇵🇱 and so on.
-export const countryCodeToFlagEmoji = str => Array.from(countryCodeToCountry(str)).map(letterToLetterEmoji).join('');

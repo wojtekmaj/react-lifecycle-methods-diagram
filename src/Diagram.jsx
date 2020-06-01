702227f6a9cd3cf0952bc6eaf987fbd5e516d798
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-06-01 14:37:52 +0200

    Replace Parcel with Webpack 5

--- src/Diagram.jsx
+++ src/Diagram.jsx
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types';
 import { isReactVersion } from './propTypes';
 
 const diagramVersions = {
-  16.3: import('./versions/16.3'),
-  16.4: import('./versions/16.4'),
+  16.3: () => import(/* webpackChunkName: "16.3" */ './versions/16.3'),
+  16.4: () => import(/* webpackChunkName: "16.4" */ './versions/16.4'),
 };
 
 export default function Diagram({ advanced, reactVersion }) {
   const [diagramElements, setDiagramElements] = useState();
 
   function loadDiagramElements() {
-    diagramVersions[reactVersion].then(setDiagramElements);
+    diagramVersions[reactVersion]().then(setDiagramElements);
   }
 
   useEffect(loadDiagramElements, [reactVersion]);

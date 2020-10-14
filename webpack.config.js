59041746c3e37482cad25cdd1909a72f30a8251a
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-10-14 12:05:05 +0200

    Update Webpack to 5.0.0-beta.30

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
     filename: '[name].[chunkhash:8].js',
     chunkFilename: '[name].[chunkhash:8].js',
   },

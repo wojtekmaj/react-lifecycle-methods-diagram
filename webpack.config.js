2f58bbe030acd3e1744e6d42bded25dbfe2aa2cd
Pop Marius <mariuspop@pitechnologies.ro>
2021-01-26 21:05:26 +0200

    Added Romanian translations.

--- webpack.config.js
+++ webpack.config.js
@@ -93,7 +93,7 @@ module.exports = {
     compress: true,
     historyApiFallback: true,
     host: '0.0.0.0',
-    port: 3000,
+    port: 3003,
     static: {
       directory: path.join(__dirname, 'dist'),
     },

efc3907c8c8f791e612aedeccbd1c3a1b1a7e277
Wojciech Maj <kontakt@wojtekmaj.pl>
2021-12-23 09:21:21 +0100

    Minor formatting

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -4,10 +4,11 @@ function isUppercase(letter) {
 
 // Splits camelCase with ZWJ characters to help with line breaking
 export function splitUpperCase(str) {
-  return (str
-    ? str
-      .split('')
-      .reduce((res, letter) => res + (isUppercase(letter) ? `\u00ad${letter}` : letter), '')
-    : str
-  );
+  if (!str) {
+    return str;
+  }
+
+  return str
+    .split('')
+    .reduce((res, letter) => res + (isUppercase(letter) ? `\u00ad${letter}` : letter), '');
 }
